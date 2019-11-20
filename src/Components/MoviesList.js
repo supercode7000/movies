72008942735a7ecc9a8ce163412d631f1ea47a00
supercode7000 <anass@super-code.de>
2019-11-20 15:28:43 +0100

    filter AtoZ ZtoA rate

--- src/Components/MoviesList.js
+++ src/Components/MoviesList.js
@@ -28,6 +28,34 @@ class MoviesList extends Component {
         const descendingMovies = this.state.movies.sort((x, y) => y.year - x.year)
         this.setState({ movies: descendingMovies })
     }
+    handleRate = () => {
+        const movies = this.state.movies.sort((x, y) => y.rate - x.rate)
+        this.setState({ movies: movies })
+    }
+    handleAToZ = () => {
+        const movies = this.state.movies.sort(function (a, b) {
+            var textA = a.title.toUpperCase();
+            var textB = b.title.toUpperCase();
+            if (textA < textB)
+                return -1
+            if (textA > textB)
+                return 1
+            return 0
+        });
+        this.setState({ movies })
+    }
+    handleZToA = () => {
+        const movies = this.state.movies.sort(function (a, b) {
+            var textA = a.title.toUpperCase();
+            var textB = b.title.toUpperCase();
+            if (textA < textB)
+                return 1
+            if (textA > textB)
+                return -1
+            return 0
+        });
+        this.setState({ movies })
+    }
     render() {
         const allMovies = this.state.movies.map(movie =>
             <MovieItem key={uuid()} movies={movie} />)
@@ -36,6 +64,9 @@ class MoviesList extends Component {
                 <div className="button">
                     <button onClick={this.handleAscending}> Sort by Date Ascending</button>
                     <button onClick={this.handleDescending}> Sort by Date Descending</button>
+                    <button onClick={this.handleRate}>  Best Rate</button>
+                    <button onClick={this.handleAToZ}>  A-Z</button>
+                    <button onClick={this.handleZToA}> Z-A</button>
                 </div>
                 <section>
                     {allMovies}
