c19c4a20d90813aa4a2b45bc9e6015aa1459da7d
supercode7000 <anass@super-code.de>
2019-11-20 14:02:34 +0100

    sfc to class componnent

--- src/Components/MoviesList.js
+++ src/Components/MoviesList.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import MovieItem from './MovieItem';
 import movies from '../data/dataMovies'
 import './moviesList.css'
 import uuid from 'react-uuid'
 
 
-const MoviesList = () => {
-    const allMovies = movies.map(movie => <MovieItem key={uuid()} movies={movie} />)
-    return (
-        <section>
-            {allMovies}
-        </section>
-    );
+// const MoviesList = () => {
+//     const allMovies = movies.map(movie => <MovieItem key={uuid()} movies={movie} />)
+//     return (
+//         <section>
+//             {allMovies}
+//         </section>
+//     );
+// }
+class MoviesList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { movies: movies }
+    }
+    render() {
+        const allMovies = this.state.movies.map(movie =>
+            <MovieItem key={uuid()} movies={movie} />)
+        return (
+            <section>
+                {allMovies}
+            </section>
+        );
+    }
 }
+
 
 export default MoviesList;
